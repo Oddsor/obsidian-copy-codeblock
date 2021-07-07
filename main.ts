7bc3af66b695ecb5a79c4ac7cfb81dc3e6442ff1
Odd Andreas Sørsæther <odd.andreas@ardoq.com>
2021-07-07 22:46:52 +0200

    Remove trailing newline from copied content

--- main.ts
+++ main.ts
@@ -1,31 +1,31 @@
 import { Notice, Plugin } from 'obsidian';
 
 const copiedCodeBlock = () => {
-	new Notice('Code copied!');
+    new Notice('Code copied!');
 };
 
 export default class MyPlugin extends Plugin {
-	async onload() {
-		console.log('loading plugin: copy code block');
+    async onload() {
+        console.log('loading plugin: copy code block');
 
-		this.registerMarkdownPostProcessor(async (el, ctx) => {
-			if (el.children.length === 1 && el.children[0].tagName === 'PRE') {
-				const content = el.textContent;
-				el.addClass('codeblock-with-copy-button');
-				const copyButton = el.createDiv();
-				copyButton.addClass('codeblock-copy-button');
-				copyButton.setText('Copy');
-				copyButton.onclick = (ev: any) => {
-					console.log('Click!', content);
-					navigator.clipboard.writeText(content);
-					copiedCodeBlock();
-				}
-			}
+        this.registerMarkdownPostProcessor(async (el, ctx) => {
+            if (el.children.length === 1 && el.children[0].tagName === 'PRE') {
+                const content = el.textContent;
+                el.addClass('codeblock-with-copy-button');
+                const copyButton = el.createDiv();
+                copyButton.addClass('codeblock-copy-button');
+                copyButton.setText('Copy');
+                copyButton.onclick = _ => {
+                    // This regex replace removes a trailing newline that 
+                    // is automatically added to textContent in code blocks
+                    navigator.clipboard.writeText(content.replace(/\n$/, ""));
+                    copiedCodeBlock();
+                }
+            }
         });
-	}
+    }
 
-	onunload() {
-		console.log('unloading plugin: copy code block');
-		
-	}
+    onunload() {
+        console.log('unloading plugin: copy code block');
+    }
 }
